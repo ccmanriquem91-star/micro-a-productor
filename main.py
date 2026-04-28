c774e080e7098e1d9b30aae4762e6289d4bbcc0f
ccmanriquem91-star <ccmanriquem91@gmail.com>
2026-04-28 17:40:38 -0500

    Add FastAPI endpoint to send data to Microservice B

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
+import os, requests
+from fastapi import FastAPI
 
+app = FastAPI()
+NOTIF_SERVICE_URL = os.getenv("NOTIF_SERVICE_URL") # URL interna de Railway
+
+@app.post("/enviar")
+def enviar_dato(nombre: str):
+    # Envía el dato al Microservicio B
+    response = requests.post(f"{NOTIF_SERVICE_URL}/procesar", json={"user": nombre})
+    return {"status": "Enviado a Micro B", "respuesta_B": response.json()}
